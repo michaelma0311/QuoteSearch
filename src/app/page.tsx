08a4c1c288c25eb3ec29fb49b32b3e50cddfa001
Michael <michaelma0311@gmail.com>
2026-04-01 13:58:29 -0700

    April fools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ type QueryResponse =
 
 export default function HomePage() {
   const [quote, setQuote] = useState(
-    "it was the best of times, it was the worst of times"
+    "AR X DK"
   );
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState<QueryResponse | null>(null);
@@ -65,7 +65,7 @@ export default function HomePage() {
       <div className="row">
         <div className="card">
           <div style={{ marginBottom: 10, color: "var(--muted)" }}>
-            Quote - Spelling should be somewhat accurate please :)
+            Aarush looking mad fine rn :)
           </div>
           <textarea
             className="textarea"
