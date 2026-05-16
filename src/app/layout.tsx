f0a4cf476053400204d51a09443e93ab72e93f8d
Michael <michaelma0311@gmail.com>
2026-05-16 10:40:44 -0700

    Fixed metadata v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "QuoteSearch",
-  description: "Get most accurate book-specific"
+  description: "Get most accurate book specific page number for quotes"
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
